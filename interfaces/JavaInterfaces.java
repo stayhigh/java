e8c20f3232c01a37515db220533b35244032ccdf
john.wang <john.wang@gandi.net>
2016-03-01 01:10:05 +0800

    java interface sample code

---
+++ interfaces/JavaInterfaces.java
@@ -0,0 +1,22 @@
+/*
+ * interface vs class:
+ * An interface is not a class. Writing an interface is similar to writing a class, but they are two different concepts. A class describes the attributes and behaviors of an object. An interface contains behaviors that a class implements
+ *
+ * interface features:
+ *
+ * - you can not instantiate an interface
+ *   - an interface does not contain any constructor
+ *   - all of the methods in an interface are abstract
+ *   - an interface cannot contain instance field. The only fields that can appear in an interface must be declared both static and final
+ *   - An interface is not extended by a class; it is implemented by a class.
+ *   - An interface can extend multiple interfaces.
+ * */
+
+
+interface JavaInterfaces{
+    final int final_int_field = 10;
+    static int static_int_field = 2;
+    abstract public void abstract_pub_method();
+    //abstract protected void abstract_pro_method();
+    //abstract private void abstract_pro_method();
+}
---
+++ interfaces/Main.java
@@ -0,0 +1,15 @@
+class Main{
+    public static void main(String [] args){
+    }
+}
+
+class testclass implements JavaInterfaces{
+
+    public void abstract_pub_method(){
+        System.out.println("Hello abstract_pub_method of testclass");
+    }
+
+    public void abstract_pro_method(){
+        System.out.println("Hello abstract_pro_method of testclass");
+    }
+}
